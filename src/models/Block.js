98ba9b6bb6e13c8b7ae941fb5c7a310bd954b365
neineigeneinei <132676894+neineigeneinei@users.noreply.github.com>
2023-05-10 21:18:44 +0800

    好

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,9 +1,17 @@
-class Block {
-  // 1. 完成构造函数及其参数
-  /* 构造函数需要包含
+import blockchain from "./Blockchain.js";
 
-  */
-  constructor() {}
+class Block {
+    // 1. 完成构造函数及其参数
+    /* 构造函数需要包含
+    */
+    constructor(blockchain,previousHash,hash,data) {
+        this.blockchain = blockchain;
+        this.data = data;
+        this.previousHash = previousHash.toString();
+        this.hash = hash;
+    }
+
 }
 
-export default Block
+
+  export default Block
--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -6,17 +6,21 @@ class Blockchain {
       - 创世区块
       - 存储区块的映射
   */
-  constructor() {}
+  constructor(name) {
+    this.name=name
+    this.genesisBlock=null
+    this.blocks=[]
+  }
+
 
   // 2. 定义 longestChain 函数
   /* 
     返回当前链中最长的区块信息列表
   */
   longestChain() {
-
-
-    return []
+    let blocks = Object.values(this.blocks);
+    blocks.sort((a, b) => a.height - b.height);
+    return blocks;
   }
 }
-
 export default Blockchain
