8edb2fb677b204ba6506f1af32ce17e2cbc25577
neineigeneinei <132676894+neineigeneinei@users.noreply.github.com>
2023-05-19 19:03:50 +0800

    lesson1

--- src/models/Block.js
+++ src/models/Block.js
@@ -11,6 +11,38 @@ class Block {
         this.hash = hash;
     }
 
+<<<<<<< Updated upstream
+=======
+  constructor(blockchain, previousHash, height, data) {
+    this.blockchain = blockchain;
+    this.previousHash = previousHash;
+    this.height = height;
+    this.data = data;
+    this.nonce = 0;
+    this.hash = this.calculateHash();
+  }
+
+  calculateHash() {
+    return sha256(
+        this.previousHash +
+        this.height +
+        this.data +
+        this.nonce
+    ).toString();
+  }
+
+  isValid() {
+    return (
+        this.hash.substring(0, DIFFICULTY) ===
+        '0'.repeat(DIFFICULTY)
+    );
+  }
+
+  setNonce(nonce) {
+    this.nonce = nonce;
+    this.hash = this.calculateHash();
+  }
+>>>>>>> Stashed changes
 }
 
 
--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -1,5 +1,8 @@
 // Blockchain
+import Block from "./Block.js";
+import {maxBy, prop, reduce, reverse, unfold, values} from "ramda";
 class Blockchain {
+<<<<<<< Updated upstream
   // 1. 完成构造函数及其参数
   /* 构造函数需要包含 
       - 名字
@@ -12,15 +15,38 @@ class Blockchain {
     this.blocks=[]
   }
 
+=======
+  constructor(name) {
+    this.name = name
+    this.blocks = {}
+  }
 
-  // 2. 定义 longestChain 函数
-  /* 
-    返回当前链中最长的区块信息列表
-  */
+  addBlock(block) {
+    if (block.isValid()) {
+      this.blocks[block.hash] = block
+    }
+  }
+>>>>>>> Stashed changes
+
   longestChain() {
+<<<<<<< Updated upstream
     let blocks = Object.values(this.blocks);
     blocks.sort((a, b) => a.height - b.height);
     return blocks;
+=======
+    let maxHeight = Math.max(...Object.values(this.blocks).map(block => block.height))
+    let currentBlock = Object.values(this.blocks).find(block => block.height === maxHeight)
+    let longestChain = []
+
+    while (currentBlock) {
+      longestChain.push(currentBlock)
+      currentBlock = Object.values(this.blocks).find(
+          (block) => block.hash === currentBlock.previousHash
+      )
+    }
+
+    return longestChain.reverse()
+>>>>>>> Stashed changes
   }
 }
 export default Blockchain
