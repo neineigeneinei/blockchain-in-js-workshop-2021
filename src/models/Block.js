b600a8fa992a98d8fe387e1475469c4f7d9a77f4
neineigeneinei <132676894+neineigeneinei@users.noreply.github.com>
2023-06-02 09:57:27 +0800

    lesson4

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,15 +1,47 @@
 import sha256 from 'crypto-js/sha256.js'
-
-export const DIFFICULTY = 2
+import UTXOPool from "./UTXOPool.js";
+export const DIFFICULTY = 3
 
 class Block {
   // 1. 完成构造函数及其参数
 
-  constructor() {}
 
-  isValid() {}
 
-  setNonce(nonce) {}
+
+  constructor(blockchain,previousHash,height,hash,coinbaseBeneficiary) {
+    this.blockchain = blockchain;
+    this.hash=hash
+    this.previousHash = previousHash.toString();
+    this.height = height;
+    this.coinbaseBeneficiary = coinbaseBeneficiary
+    this.utxoPool = new UTXOPool()
+    this.transactions = {}
+  }
+
+  isValid() {
+    return  this.hash === this.calculateHash()&&
+        (this.hash.substring(0,DIFFICULTY) ==='0'.repeat(DIFFICULTY))
+  }
+
+  setNonce(nonce) {
+    this.nonce=nonce
+    this.setHash()
+  }
+
+  calculateHash(){
+    return sha256(
+      this.nonce+
+        this.previousHash+
+        this.height+
+        this.coinbaseBeneficiary+
+        this.transactions
+    ).toString();
+  }
+  setHash(){
+    this.hash = this.calculateHash()
+  }
+
+
 }
 
 export default Block
